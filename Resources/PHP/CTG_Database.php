e883e0a74928a8a48b82e3245843368385a4de1c
Tom Yu <yutom3@yahoo.com>
2017-12-21 21:15:59 -0800

    Changed isset statements to !empty

--- Resources/PHP/CTG_Database.php
+++ Resources/PHP/CTG_Database.php
@@ -1,28 +1,22 @@
 <?php
 	// Update Schedule.txt
-	if (isset($_POST[game1]) && isset($_POST[game2]) && isset($_POST[game3])) {
+	if (!empty($_POST[game1]) && !empty($_POST[game2]) && !empty($_POST[game3])) {
 		$content = "<div class='row'><div class='col-sm-4 ticker_data' id='game1'>".$_POST[game1]."</div><div class='col-sm-4 ticker_data' id='game2'>".$_POST[game2]."</div><div class='col-sm-4 ticker_data' id='game3'>".$_POST[game3]."</div></div>";
 		echo file_put_contents("../TXT/Schedule.txt",$content);
 		}
 
-	// Update Anoouncements.txt
-  if (isset($_POST[announcement1])) {
-    $start = 1;
-  } else if (isset($_POST[announcement2])) {
-    $start = 2;
-  }
-  switch ($start) {
-    case 1:
-      $content = $_POST[announcement1];
-      if (isset($_POST[announcement2])) {
-        $content = $content."*".$_POST[announcement2];
-      }
-      break;
-    case 2:
-      $content = $_POST[announcemnt2];
-      break;
-  }
-  if (isset($_POST[announcement3])) {
+	// Update Anouncements.txt
+  if (!empty($_POST[announcement1])) {
+		$content = $_POST[announcement1];
+		if (!empty($_POST[announcement2])) {
+			$content = $content."*".$_POST[announcement2];
+		}
+  } else if (!empty($_POST[announcement2])) {
+		$content = $_POST[announcemnt2];
+  } else if (!empty($_POST[announcement3])) {
+		$content = $_POST[announcemnt3];
+	}
+	if (!empty($_POST[announcement3])) {
     $content = $content."*".$_POST[announcement3];
   }
   echo file_put_contents("../TXT/Announcements.txt",$content);

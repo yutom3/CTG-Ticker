8c330f201243a96eb6683abb5a0ce880205dbd45
arthurtham <19481191+arthurtham@users.noreply.github.com>
2017-12-22 16:37:13 -0800

    Split Schedule, Fixed Boolean Logic
    Split now and next/after into two lines. Fixed error with logic
    preventing announcements from appearing correctly. Added "Sponsor" next
    to sponsors.

--- Resources/PHP/CTG_Database.php
+++ Resources/PHP/CTG_Database.php
@@ -1,15 +1,15 @@
 <?php
 	// Update Schedule.txt
 	if (!empty($_POST[game1]) && !empty($_POST[game2]) && !empty($_POST[game3]) && !empty($_POST[time2]) && !empty($_POST[time3])) {
-		$content = "<div class='row'><div class='col-sm-4' id='game1'><div class='tickerTitle'>NOW</div><div class='tickerData'>" . $_POST[game1] . "</div></div><div class='col-sm-4' id='game2'><div class='tickerTitle'>NEXT</div><div class='tickerData'>".$_POST[time2]." - ".$_POST[game2]."</div></div><div class='col-sm-4' id='game3'><div class='tickerTitle'>AFTER</div><div class='tickerData'>".$_POST[time3]." - ".$_POST[game3]."</div></div></div>";
+        $content = "<div class='row'><div class='col-sm-4' id='game1'><div class='tickerTitle'>Now Playing</div><div class='tickerData'>" . $_POST[game1] . "</div></div></div>*<div class='row'><div class='col-sm-4' id='game2'><div class='tickerTitle'>Next Games</div><div class='tickerData'>".$_POST[time2]." - ".$_POST[game2]." | ".$_POST[time3]." - ".$_POST[game3]."</div></div></div>";
 		echo file_put_contents("../TXT/Schedule.txt",$content);
 		}
 
 	// Update Anouncements.txt
   if (!empty($_POST[announcement1])) {
-		$content = $_POST[announcement1];
+        $content = "<div class='tickerTitle'>Latest News </div><div class='tickerData'>" . $_POST[announcement1] . "</div>";
 		if (!empty($_POST[announcement2])) {
-			$content = $content."*".$_POST[announcement2];
+			$content .= "*<div class='tickerTitle'>Latest News </div><div class='tickerData'>".$_POST[announcement2] . "</div>";
 		}
   } else if (!empty($_POST[announcement2])) {
 		$content = $_POST[announcemnt2];
@@ -17,7 +17,8 @@
 		$content = $_POST[announcemnt3];
 	}
 	if (!empty($_POST[announcement3])) {
-    $content = $content."*".$_POST[announcement3];
+    $content .= "*<div class='tickerTitle'>Latest News </div><div class='tickerData'>".$_POST[announcement3] . "</div>";
   }
   echo file_put_contents("../TXT/Announcements.txt",$content);
 ?>
+
